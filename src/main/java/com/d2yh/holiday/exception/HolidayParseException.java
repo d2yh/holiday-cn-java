19796fa4c48bbb19a33d9d73814e2092c3d0d94d
Heng Yao <30646718+d2yh@users.noreply.github.com>
2026-07-17 10:37:21 +0800

    Add parse exception class

---
+++ src/main/java/com/d2yh/holiday/exception/HolidayParseException.java
@@ -0,0 +1,15 @@
+package com.d2yh.holiday.exception;
+
+/**
+ * Exception thrown when parsing holiday data fails
+ */
+public class HolidayParseException extends HolidayException {
+    
+    public HolidayParseException(String message) {
+        super(message);
+    }
+    
+    public HolidayParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
