e858dad206d0ebbca724c01c368bb19709f07985
Heng Yao <30646718+d2yh@users.noreply.github.com>
2026-07-17 10:37:35 +0800

    Add fetch exception class

---
+++ src/main/java/com/d2yh/holiday/exception/HolidayFetchException.java
@@ -0,0 +1,15 @@
+package com.d2yh.holiday.exception;
+
+/**
+ * Exception thrown when fetching holiday data fails
+ */
+public class HolidayFetchException extends HolidayException {
+    
+    public HolidayFetchException(String message) {
+        super(message);
+    }
+    
+    public HolidayFetchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
