adc39470b30ba001601c96cb0c66313fc14a5201
Heng Yao <30646718+d2yh@users.noreply.github.com>
2026-07-17 10:38:34 +0800

    Add custom exception class

---
+++ src/main/java/com/d2yh/holiday/exception/HolidayException.java
@@ -0,0 +1,15 @@
+package com.d2yh.holiday.exception;
+
+/**
+ * Base exception for holiday-cn-java
+ */
+public class HolidayException extends RuntimeException {
+    
+    public HolidayException(String message) {
+        super(message);
+    }
+    
+    public HolidayException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
